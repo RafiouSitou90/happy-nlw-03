892da5c7d14dd4ee19882103525eeaece01cad0c
Rafiou Sitou <rafiousitou90@gmail.com>
2020-10-12 13:20:06 -0300

    feat(route): Add App Routes component
    - Add routes.css
    - Add Landing page
    - Add OrphanagesMap page
    - Add react-router-dom package
    - Update App.tsx

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FiArrowRight } from "react-icons/fi";
+import { Link } from 'react-router-dom';
+
+import logo from "../assets/images/logo.svg";
+
+const Landing = () => {
+    return (
+        <div id="page-landing">
+            <div className="content-wrapper">
+                <img src={logo} alt="Happy"/>
+
+                <main>
+                    <h1>Bring happiness to the world</h1>
+                    <p>Visit orphanages and change the day of many children.</p>
+                </main>
+
+                <div className="location">
+                    <strong>Federal District</strong>
+                    <span>Brasilia</span>
+                </div>
+
+                <Link to="/app" className="enter-app">
+                    <FiArrowRight size={26} color={"rgba(0, 0, 0, 0.6)"} />
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Landing;
---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const OrphanagesMap = () => {
+
+  return (
+      <h1>Orphanages Map</h1>
+  );
+};
+
+export default OrphanagesMap;
