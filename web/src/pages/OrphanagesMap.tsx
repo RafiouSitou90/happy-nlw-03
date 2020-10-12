ebf163a838f1bd5cede88d07c85238a216a69fcf
Rafiou Sitou <rafiousitou90@gmail.com>
2020-10-12 15:06:04 -0300

    feat(page): Update OrphanagesMap page
    - Add leaflet and react-leaflet packages
    - Add .env file
    - Update orphanages-map.css
    - Update OrphanagesMap page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,36 +1,51 @@
+import { LatLng } from "leaflet";
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
 
 import mapMarker from '../assets/images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css'
+import 'leaflet/dist/leaflet.css'
 
 const OrphanagesMap = () => {
+    const position = new LatLng(-15.8665877, -47.9713124)
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarker} alt="Happy"/>
 
-  return (
-      <div id="page-map">
-          <aside>
-              <header>
-                  <img src={mapMarker} alt="Happy"/>
+                    <h2>Choose an orphanage on the map</h2>
+                    <p>Many children are waiting for your visit :)</p>
+                </header>
 
-                  <h2>Choose an orphanage on the map</h2>
-                  <p>Many children are waiting for your visit :)</p>
-              </header>
+                <footer>
+                    <strong>Federal District</strong>
+                    <span>Brasilia</span>
+                </footer>
+            </aside>
 
-              <footer>
-                  <strong>Federal District</strong>
-                  <span>Brasilia</span>
-              </footer>
+            <Map
+                center={position}
+                zoom={15}
+                style={{
+                    width: '100%',
+                    height: '100%'
+                }}
+            >
+                {/*<TileLayer url={"https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"}/>*/}
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+            </Map>
 
-              <div></div>
-
-              <Link to={""} className="create-orphanage">
-                  <FiPlus size={32} color={"#FFF"} />
-              </Link>
-          </aside>
-      </div>
-  );
+            <Link to={""} className="create-orphanage">
+                <FiPlus size={32} color={"#FFF"}/>
+            </Link>
+        </div>
+    );
 };
 
 export default OrphanagesMap;
