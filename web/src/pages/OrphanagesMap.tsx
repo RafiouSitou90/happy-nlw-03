72851f8bc33bb644b1a49a2a372c71b5e15d1f99
Rafiou Sitou <rafiousitou90@gmail.com>
2020-10-12 13:44:11 -0300

    feat(page): Update OrphanagesMap page
    - Add orphanages-map.css
    - Update OrphanagesMap page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { FiPlus } from 'react-icons/fi'
+
+import mapMarker from '../assets/images/map-marker.svg'
+
+import '../styles/pages/orphanages-map.css'
 
 const OrphanagesMap = () => {
 
   return (
-      <h1>Orphanages Map</h1>
+      <div id="page-map">
+          <aside>
+              <header>
+                  <img src={mapMarker} alt="Happy"/>
+
+                  <h2>Choose an orphanage on the map</h2>
+                  <p>Many children are waiting for your visit :)</p>
+              </header>
+
+              <footer>
+                  <strong>Federal District</strong>
+                  <span>Brasilia</span>
+              </footer>
+
+              <div></div>
+
+              <Link to={""} className="create-orphanage">
+                  <FiPlus size={32} color={"#FFF"} />
+              </Link>
+          </aside>
+      </div>
   );
 };
 
