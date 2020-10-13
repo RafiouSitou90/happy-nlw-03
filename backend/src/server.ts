18d05f15a50117bc11c9a865c6e6298e03c7e503
Rafiou Sitou <rafiousitou90@gmail.com>
2020-10-13 12:46:42 -0300

    feat(database): Create Orphanage routes in server.ts
    - Update server.ts
    - Update ormconfig.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
+import { getRepository } from 'typeorm'
 
 import './database/connection'
+import Orphanage from './models/Orphanage';
 
 const APP_PORT = 3333;
 const APP_SERVER = '127.0.0.1';
@@ -8,6 +10,33 @@ const APP_SERVER = '127.0.0.1';
 const app = express()
 
 app.use(express.json())
+
+app.post('/orphanages', async (request, response) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    })
+    await orphanagesRepository.save(orphanage).then(_ => true)
+
+    return response.status(201).json(orphanage)
+})
 
 app.listen(APP_PORT, () => {
     console.log('App is running and listening on %s:%d',  APP_SERVER, APP_PORT)
