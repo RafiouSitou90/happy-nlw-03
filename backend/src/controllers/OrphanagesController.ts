4f2251b5a86d17e7b6ff265aa165808eee1c0b7f
Rafiou Sitou <rafiousitou90@gmail.com>
2020-10-13 14:34:18 -0300

    feat(models): Add Image model
    - Add Image.ts model
    - Update Orphanage.ts model
    - Update OrphanagesController.ts

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -33,6 +33,11 @@ export default {
 
         const orphanagesRepository = getRepository(Orphanage)
 
+        const requestImages = request.files as Express.Multer.File[]
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
+
         const orphanage = orphanagesRepository.create({
             name,
             latitude,
@@ -40,7 +45,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
         })
         await orphanagesRepository.save(orphanage).then(_ => true)
 
