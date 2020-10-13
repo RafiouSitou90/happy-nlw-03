28eed52b4f86a4f9ec5b1a61d465fd41b084ef18
Rafiou Sitou <rafiousitou90@gmail.com>
2020-10-13 18:15:22 -0300

    feat(validation): Add Orphanages validation.
    - Add error handler.ts
    - Update OrphanagesController.ts
    - Update server.ts
    - Add cors, and yup packages

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,6 +1,21 @@
 import { ErrorRequestHandler } from 'express'
+import { ValidationError } from 'yup'
+
+interface ValidationErrors {
+    [key: string]: string[]
+}
 
 const errorHandler: ErrorRequestHandler = (error, _request, response, _next) => {
+    if (error instanceof ValidationError) {
+        let errors: ValidationErrors = {}
+
+        error.inner.forEach(err => {
+            errors[err.path] = err.errors
+        })
+
+        return response.status(400).json({ message: 'Validation errors', errors })
+    }
+
     console.error(error)
 
     return response.status(500).json({ message: 'Internal server error' })
