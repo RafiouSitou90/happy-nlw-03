19e000e5f8128826144f52165db0b2365a13cff5
Rafiou Sitou <rafiousitou90@gmail.com>
2020-10-13 12:56:29 -0300

    feat(controller): Create OrphanagesController
    - Add OrphanagesController
    - Add routes.ts
    - Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,7 @@
 import express from 'express'
-import { getRepository } from 'typeorm'
 
 import './database/connection'
-import Orphanage from './models/Orphanage';
+import routes from "./routes"
 
 const APP_PORT = 3333;
 const APP_SERVER = '127.0.0.1';
@@ -10,33 +9,7 @@ const APP_SERVER = '127.0.0.1';
 const app = express()
 
 app.use(express.json())
-
-app.post('/orphanages', async (request, response) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = request.body
-
-    const orphanagesRepository = getRepository(Orphanage)
-
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    })
-    await orphanagesRepository.save(orphanage).then(_ => true)
-
-    return response.status(201).json(orphanage)
-})
+app.use(routes)
 
 app.listen(APP_PORT, () => {
     console.log('App is running and listening on %s:%d',  APP_SERVER, APP_PORT)
