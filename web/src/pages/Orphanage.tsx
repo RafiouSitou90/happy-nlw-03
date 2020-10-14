896b519b3afc2bcea8a2d6772075e248ffe0b938
Rafiou Sitou <rafiousitou90@gmail.com>
2020-10-14 15:12:38 -0300

    feat(web): Update Orphanage adding load orphanage detail from the api
    - Update Orphanage.tsx
    - Update orphanage.css

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -1,14 +1,58 @@
-import React from 'react'
+import {LatLng} from "leaflet";
+import React, {useEffect, useState} from 'react'
 import { FaWhatsapp } from 'react-icons/fa'
 import { FiClock, FiInfo } from 'react-icons/fi'
 import { Map, Marker, TileLayer } from 'react-leaflet'
+import { useParams } from 'react-router-dom'
+
 
 import { Sidebar } from '../components'
+import {api} from "../services";
 import {mapIcon} from '../utils'
 
 import '../styles/pages/orphanage.css'
 
+interface Orphanage {
+    name: string
+    about: string
+    latitude: number
+    longitude: number
+    instructions: string
+    opening_hours: string
+    open_on_weekends: boolean
+    images: Array<{url: string}>
+}
+
+interface OrphanageParams {
+    id: string
+}
+
 export default function Orphanage() {
+    const { id } = useParams<OrphanageParams>()
+    const [orphanage, setOrphanage] = useState<Orphanage>()
+    const [activeImageIndex, setActiveImageIndex] = useState<number>(0)
+
+    useEffect(() => {
+        api.get(`orphanages/${id}`).then(response => {
+            setOrphanage(response.data)
+        })
+    }, [id])
+
+    if (!orphanage) {
+        return <p>Loading...</p>
+    }
+
+    const {
+        images,
+        name,
+        about,
+        latitude,
+        longitude,
+        instructions,
+        open_on_weekends,
+        opening_hours
+    } = orphanage
+    const position = new LatLng(latitude, longitude)
 
     return (
         <div id="page-orphanage">
@@ -16,38 +60,34 @@ export default function Orphanage() {
 
             <main>
                 <div className="orphanage-details">
-                    <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+                    <img src={images[activeImageIndex].url} alt={name} />
 
                     <div className="images">
-                        <button className="active" type="button">
-                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-                        </button>
-                        <button type="button">
-                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-                        </button>
-                        <button type="button">
-                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-                        </button>
-                        <button type="button">
-                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-                        </button>
-                        <button type="button">
-                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-                        </button>
-                        <button type="button">
-                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-                        </button>
+                        {
+                            images.map(({url}, index) => {
+                                return (
+                                    <button
+                                        key={index}
+                                        className={activeImageIndex === index ? 'active' : ''}
+                                        type="button"
+                                        onClick={() => {
+                                            setActiveImageIndex(index)
+                                        }}
+                                    >
+                                        <img src={url} alt={name} />
+                                    </button>
+                                )
+                            })
+                        }
                     </div>
 
                     <div className="orphanage-details-content">
-                        <h1>Child House</h1>
-                        <p>
-                            It provides assistance to children aged 6 to 15 who are at risk and / or socially vulnerable.
-                        </p>
+                        <h1>{name}</h1>
+                        <p>{about}</p>
 
                         <div className="map-container">
                             <Map
-                                center={[-27.2092052,-49.6401092]}
+                                center={position}
                                 zoom={16}
                                 style={{ width: '100%', height: 280 }}
                                 dragging={false}
@@ -59,36 +99,54 @@ export default function Orphanage() {
                                 <TileLayer
                                     url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                                 />
-                                <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} />
+                                <Marker interactive={false} icon={mapIcon} position={position} />
                             </Map>
 
                             <footer>
-                                <a href="">View directions on Google Maps</a>
+                                <a
+                                    href={`https://www.google.com/maps/dir/.api=1&destination=${latitude},${longitude}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    View directions on Google Maps
+                                </a>
                             </footer>
                         </div>
 
                         <hr />
 
                         <h2>Visiting instructions</h2>
-                        <p>Come as you feel more at ease and bring a lot of love to give.</p>
+                        <p>{instructions}</p>
 
                         <div className="open-details">
                             <div className="hour">
                                 <FiClock size={32} color="#15B6D6" />
                                 Monday to Friday <br />
-                                8AM to 6PM
+                                {opening_hours}
                             </div>
-                            <div className="open-on-weekends">
-                                <FiInfo size={32} color="#39CC83" />
-                                We serve <br />
-                                weekend
-                            </div>
+                            {open_on_weekends
+                                ? (
+                                    <div className="open-on-weekends">
+                                        <FiInfo size={32} color="#39CC83" />
+                                        We serve <br/>
+                                        weekend
+                                    </div>
+                                )
+                                : (
+                                    <div className="open-on-weekends dont-open">
+                                        <FiInfo size={32} color="#FF669D"/>
+                                        We don't serve <br/>
+                                        weekend
+                                    </div>
+                                )
+                            }
+
                         </div>
 
-                        <button type="button" className="contact-button">
+{/*                        <button type="button" className="contact-button">
                             <FaWhatsapp size={20} color="#FFF" />
                             Get in touch
-                        </button>
+                        </button>*/}
                     </div>
                 </div>
             </main>
