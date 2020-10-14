f3551f52a29a995e265517b00af183a57792998d
Rafiou Sitou <rafiousitou90@gmail.com>
2020-10-14 12:46:25 -0300

    feat(web): Update OrphanagesMap adding Marker and Popup
    - Update OrphanagesMap.tsx
    - Update orphanages-map.css
    - Update map-marker.svg

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,20 @@
-import { LatLng } from "leaflet";
+import Leaflet, { LatLng } from "leaflet";
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FiPlus } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet'
+import { FiPlus, FiArrowRight } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import mapMarker from '../assets/images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css'
 import 'leaflet/dist/leaflet.css'
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarker,
+    iconSize: [48, 58],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
 const OrphanagesMap = () => {
     const position = new LatLng(-15.8665877, -47.9713124)
@@ -37,8 +44,25 @@ const OrphanagesMap = () => {
             >
                 {/*<TileLayer url={"https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"}/>*/}
                 <TileLayer
-                    url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
+                <Marker
+                    position={position}
+                    icon={mapIcon}
+                >
+                    <Popup
+                        closeButton={false}
+                        minWidth={240}
+                        maxWidth={240}
+                        className="map-popup"
+                    >
+                        Child House
+                        <Link to={""}>
+                            <FiArrowRight size={20} color={"#FFF"} />
+                        </Link>
+                    </Popup>
+                </Marker>
+
             </Map>
 
             <Link to={""} className="create-orphanage">
