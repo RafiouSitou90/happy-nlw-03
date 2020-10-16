cdf545004a3d27920c7582ea03a883d15c94493e
Rafiou Sitou <rafiousitou90@gmail.com>
2020-10-16 11:45:13 -0300

    feat(mobile): Add Header component.
    - Add Header.tsx
    - Update routes.tsx

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { Header } from './components'
 import {
 	OrphanageData,
 	OrphanageDetails,
@@ -13,17 +14,39 @@ const { Navigator, Screen } = createStackNavigator()
 const Routes = () => {
 	return (
 		<NavigationContainer>
-			<Navigator screenOptions={{ headerShown: false }}>
+			<Navigator
+				screenOptions={{
+					headerShown: false,
+					cardStyle: { backgroundColor: '#F2F3F5' },
+				}}
+			>
 				<Screen name={'OrphanagesMap'} component={OrphanagesMap} />
 				<Screen
 					name={'OrphanageDetails'}
 					component={OrphanageDetails}
+					options={{
+						headerShown: true,
+						header: () => (
+							<Header showCancel={false} title={'Orphanage'} />
+						),
+					}}
 				/>
 				<Screen
 					name={'SelectMapPosition'}
 					component={SelectMapPosition}
+					options={{
+						headerShown: true,
+						header: () => <Header title={'Select Location'} />,
+					}}
 				/>
-				<Screen name={'OrphanageData'} component={OrphanageData} />
+				<Screen
+					name={'OrphanageData'}
+					component={OrphanageData}
+					options={{
+						headerShown: true,
+						header: () => <Header title={'Enter the data'} />,
+					}}
+				/>
 			</Navigator>
 		</NavigationContainer>
 	)
